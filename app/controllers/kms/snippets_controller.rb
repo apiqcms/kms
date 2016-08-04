14b42c399a39556c7955f93cccf200ebe1196254
GarPit <garik.piton@gmail.com>
2016-08-04 11:44:02 +0300

    fix snippets issue + added help translations

--- app/controllers/kms/snippets_controller.rb
+++ app/controllers/kms/snippets_controller.rb
@@ -3,29 +3,29 @@ module Kms
     respond_to :json
 
     def index
-      respond_with Snippet.all, root: false
+      render json: Snippet.all, root: false
     end
 
     def create
       @snippet = Snippet.create(snippet_params)
-      respond_with @snippet, root: false
+      render json: @snippet, root: false
     end
 
     def update
       @snippet = Snippet.find(params[:id])
       @snippet.update(snippet_params)
-      respond_with @snippet, root: false
+      render json: @snippet, root: false
     end
 
     def show
       @snippet = Snippet.find(params[:id])
-      respond_with @snippet, root: false
+      render json: @snippet, root: false
     end
 
     def destroy
       @snippet = Snippet.find(params[:id])
       @snippet.destroy
-      respond_with @snippet
+      render json: @snippet
     end
 
     protected
