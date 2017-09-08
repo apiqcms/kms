55ed3b398ed53d843db12b26ebc80d59cf6af681
GarPit <garik.piton@gmail.com>
2017-09-08 16:50:16 +0300

    need to have this according to guide from devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,6 +9,15 @@ module Kms
         new_kms_user_session_path
       end
 
+      # You need to override respond to eliminate recall
+      def respond
+        if http_auth?
+          http_auth
+        else
+          redirect
+        end
+      end
+
     end
 
   end
